b965b21ef331615d6bd6c5416249c97cd1117983
srivnaman <82708027+srivnaman@users.noreply.github.com>
2022-01-26 20:22:36 +0530

    Create Health.sol

---
+++ Health.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.6.0;
+contract SimpleStorage{
+    uint256 myNum = 13;
+    struct People{
+        string Name;
+        uint256 Age;   
+        string Message;
+       
+    }
+    struct Prescription{
+        uint256 DocId;
+        string DocName;
+        string Advice;
+    }
+    //List of Patients and Their presciptions
+    People[] public Family; //each element refers to a particular family member
+    Prescription[] public Advices;
+    
+   // mapping(string=>uint256) public NameToAge;
+
+    People patient1 ;
+    function setRecord(string memory name, string memory pres,uint256 age) public{
+        patient1 = People(name,age,pres);
+    }
+    function giveAdivice(string memory name,uint256 id,string memory desc) public{
+        Advices.push(Prescription({DocName:name,DocId:id,Advice:desc}));
+    }
+    function retrieve() public view returns(string memory){
+        return patient1.Message;
+    }
+    function showPastPres() public view returns(string memory){
+        
+    }
+}
